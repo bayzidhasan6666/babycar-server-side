37883e7b23ae69131c01779d5b031a8b95f159bf
bayzidhasan6666 <121633867+bayzidhasan6666@users.noreply.github.com>
2023-05-18 12:53:30 +0800

    mongodb pinned

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+console.log(process.env.DB_PASS);
+
 // Mongodb
-const uri =
-  'mongodb+srv://<username>:<password>@cluster0.g4e8qzr.mongodb.net/?retryWrites=true&w=majority';
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g4e8qzr.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
