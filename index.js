8815851ea9decff958b1f372d09daee42e9b5473
bayzidhasan6666 <121633867+bayzidhasan6666@users.noreply.github.com>
2023-05-18 12:26:28 +0800

    require updated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,8 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 // Mongodb
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri =
   'mongodb+srv://<username>:<password>@cluster0.g4e8qzr.mongodb.net/?retryWrites=true&w=majority';
 
