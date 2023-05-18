31ab7715cae89b59ed4ef894f4dc0899e2c4c138
bayzidhasan6666 <121633867+bayzidhasan6666@users.noreply.github.com>
2023-05-18 12:25:03 +0800

    init mongodb

--- index.js
+++ index.js
@@ -7,6 +7,37 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Mongodb
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri =
+  'mongodb+srv://<username>:<password>@cluster0.g4e8qzr.mongodb.net/?retryWrites=true&w=majority';
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 // Define a route
 app.get('/', (req, res) => {
   res.send('Toy marketplace server is running....');
@@ -16,3 +47,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+// 8dctmrzv3vcfIyDt
+// BabyCar
